1cac8a6d04c08c0607e62449df1eee0f965599bf
tq9955613 <jp9955613@gmail.com>
2021-02-03 16:21:09 +0800

    add mirage server & add meals REST api

--- src/index.js
+++ src/index.js
@@ -3,6 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { makeServer } from "./server"
+import axios from "axios"
+
+if (process.env.NODE_ENV === "development") {
+  makeServer({ environment: "development" })
+
+  test()
+
+  async function test() {
+    let url = '/api/meals'
+
+    // get inital meals
+    getMeals()
+
+    // add test
+    let newMeal = {
+      meal: {
+        name: "新餐點",
+        calories: 999,
+      }
+    }
+    let {data: {meal: {id: newMealId}}} = await axios.post(url, newMeal)
+    getMeals()
+
+    // update test
+    let updatedMeal = {
+      meal: {
+        calories: 100
+      }
+    }
+    await axios.patch(`${url}/${newMealId}`, updatedMeal)
+    getMeals()
+
+    // delete test
+    await axios.delete(`${url}/${newMealId}`)
+    getMeals()
+
+    async function getMeals() {
+      let {data} = await axios.get(url)
+      return console.log(data)
+    }
+  }
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
